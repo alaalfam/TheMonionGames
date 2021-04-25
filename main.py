7a49d71670e3c99b759f43db331074f699ec196c
Asus <alaalfam@gmail.com>
2021-04-25 19:26:12 +0430

    define methods should work with them. we must to complete methods

---
+++ main.py
@@ -0,0 +1,26 @@
+vowels = ["A", "E", "I", "O", "U"]
+
+
+def substring_should_start_with_vowels(str):
+    pass
+
+
+def kevin_words(str):
+    list_words = substring_should_start_with_vowels(str)
+    return list_words
+
+
+def substring_should_start_with_consonants(str):
+    pass
+
+
+def stuart_words(str):
+    list_words = substring_should_start_with_consonants(str)
+
+def minion_game(str):
+    pass
+
+
+if __name__ == "__main__":
+    str = input()
+    minion_game(str)
